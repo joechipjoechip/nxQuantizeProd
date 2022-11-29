483db68cfc12bb839665dfb9f879cc158cda3b95
Lionel <joechipjoechip@gmail.com>
2022-11-29 20:46:01 +0100

    change perspective camera far to 100

--- components/sceneBuilder.js
+++ components/sceneBuilder.js
@@ -27,7 +27,7 @@ class SceneBuilder {
 
 		// _ Three elements
 		this.aspectRatio = window.innerWidth / window.innerHeight;
-		this.camera = new THREE.PerspectiveCamera(50, this.aspectRatio, 0.01, 1000);
+		this.camera = new THREE.PerspectiveCamera(50, this.aspectRatio, 0.01, 100);
 		this.camera.name = "camera";
 		this.scene = new THREE.Scene();
 		this.sequencesElements = {};
