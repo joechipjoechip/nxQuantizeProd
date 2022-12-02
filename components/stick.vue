2bcb3d065f0674734a6bad276ac629db205d1fa2
lio <joechipjoechip@gmail.com>
2022-12-02 15:00:49 +0100

    add shit to inputs for stick

--- components/stick.vue
+++ components/stick.vue
@@ -195,6 +195,7 @@
             bobInputKeysHandler( stickPos ){
 
                 const inputs = {
+                    shift: stickPos.y > 0.9,
                     forward: stickPos.y > this.inputTrigger,
                     backward: stickPos.y < this.inputTrigger * -1,
                     right: stickPos.x > this.inputTrigger,
@@ -238,82 +239,64 @@
 
 <style lang="scss" scoped>
 
-    .stick {
+.stick {
+  &_hitbox {
+    // &-left {
+    //     background-color: rgba(0,0,255,0.2);
+    // }
 
-        &_hitbox {
-        
-            // &-left {
-            //     background-color: rgba(0,0,255,0.2);
-            // }
-        
-            // &-right {
-            //     background-color: rgba(0,255,0,0.2);
-            // }
-            
-            &-left,
-            &-right {
-                // box-sizing: border-box;
-                border-radius: 50%;
-                // border: 3px solid rgba(255,255,255,0.08);
-                // margin: 2vw;
-                box-shadow: inset 0 0 4vw rgba(255,255,255,0.1S);
+    // &-right {
+    //     background-color: rgba(0,255,0,0.2);
+    // }
 
-                width: 20vw;
-                height: 20vw;
-
-                display: flex;
-                flex-flow: row nowrap;
-                justify-content: center;
-                align-items: center;
-            }
-        
-        }
-
-        &_inner {
-
-            &-circle {
-
-                &-surface {
-
-                    pointer-events: none;
-                   
-                    border-radius: 50%;
-                    width: 75%;
-                    height: 75%;
-    
-                    // background-color: rgba(255,255,255,0.15);
-                    box-shadow: inset 0 0 4vw rgba(255,255,255,0.2S);
-
-                    display: flex;
-                    flex-flow: row nowrap;
-                    justify-content: center;
-                    align-items: center;
-                    
-                }
-    
-                &-deep {
-                   
-                    border-radius: 50%;
-                    width: 72%;
-                    height: 72%;
-    
-                    background-color: rgba(255,255,255,0.1);
-                    box-shadow: 0 5px 12px rgba(255,255,255,0.3);
-                    transform: scale(1);
-                    
-                    transition: all .25s;
-                    
-                    &.active {
-                        transform: scale(1.2);
-                        background-color: rgba(255,255,255,0.3);
-                        // box-shadow: 0 0 0 transparent;
-                        box-shadow: 0px 0px 22px rgba(255,255,255,0.4);
-                    }
-    
-                }
-            }
-            
-        }
+    &-left,
+    &-right {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: center;
+      align-items: center;
+      width: 20vw;
+      height: 20vw;
+      // border: 3px solid rgba(255,255,255,0.08);
+      // margin: 2vw;
+      box-shadow: inset 0 0 4vw rgba(255,255,255,.1S);
+      // box-sizing: border-box;
+      border-radius: 50%;
     }
+  }
 
+  &_inner {
+    &-circle {
+      &-surface {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+        align-items: center;
+        width: 75%;
+        height: 75%;
+        // background-color: rgba(255,255,255,0.15);
+        box-shadow: inset 0 0 4vw rgba(255,255,255,.2S);
+        border-radius: 50%;
+        pointer-events: none;
+      }
+
+      &-deep {
+        width: 72%;
+        height: 72%;
+        background-color: rgba(255,255,255,.1);
+        box-shadow: 0 5px 12px rgba(255,255,255,.3);
+        border-radius: 50%;
+        transform: scale(1);
+        transition: all .25s;
+
+        &.active {
+          background-color: rgba(255,255,255,.3);
+          // box-shadow: 0 0 0 transparent;
+          box-shadow: 0 0 22px rgba(255,255,255,.4);
+          transform: scale(1.2);
+        }
+      }
+    }
+  }
+}
 </style>
