c40ebb287918007359ac6a38caef3423a7388d43
Lionel <joechipjoechip@gmail.com>
2023-06-29 01:24:52 +0200

    avorted attemps on mousehandler (commented)

--- components/mouseHandler.vue
+++ components/mouseHandler.vue
@@ -17,7 +17,13 @@
 		data(){
 			return {
 				core,
-				currentPosition: { x: 0, y: 0 }
+				currentPosition: { x: 0, y: 0 },
+				isMovingFromZero: false
+			}
+		},
+		watch: {
+			currentPosition( newVal ){
+				this.$nuxt.$emit("view-update-by-stick", newVal);
 			}
 		},
 		mounted(){
@@ -41,27 +47,45 @@
 					this.core.mouse.moveTimeout * 1000
 				);
 
-				this.currentPosition = this.computePosition(event);
+				// if( !this.isMovingFromZero ){
 
-				this.emitNewPosition();
+				// 	if( this.currentPosition.x === 0 && this.currentPosition.y === 0 ){
+
+				// 		this.isMovingFromZero = true;
+	
+				// 		this.positionFromZeroTo({
+				// 			x: event.screenX,
+				// 			y: event.screenY
+				// 		});
+	
+				// 	} else {
+	
+				// 		this.computePosition(event);
+						
+				// 	}
+					
+				// }
+				
+				this.computePosition(event);
+
 			},
 
 			computePosition( event ){
 
-				return {
-					x: (((event.screenX + window.innerWidth / 2) / window.innerWidth) - 1) * 2,
-					y: (((event.screenY + window.innerHeight / 2) / window.innerHeight) - 1) * -2
-				};
+				this.currentPosition = {
+					x: this.formulaX(event.screenX),
+					y: this.formulaY(event.screenY)
+				}
 
 
 			},
 
-			emitNewPosition(){
+			formulaX( screenX ){
+				return (((screenX + window.innerWidth / 2) / window.innerWidth) - 1) * 2
+			},
 
-				// console.log("mousemoveHandler : ", this.currentPosition.x);
-
-				this.$nuxt.$emit("view-update-by-stick", this.currentPosition);
-
+			formulaY( screenY ){
+				return (((screenY + window.innerHeight / 2) / window.innerHeight) - 1) * -2
 			},
 
 			positionRecenter(){
@@ -80,13 +104,41 @@
 
 						that.currentPosition = animatedObject;
 
-						that.emitNewPosition(animatedObject)
-
 					},
 					onUpdateParams: [this]
 				});
 
 			},
+
+			// positionFromZeroTo( goToObj ){
+
+			// 	const animatedObject = {
+			// 		x: 0,
+			// 		y: 0
+			// 	};
+
+			// 	const tlGoTo = new TimelineLite();
+
+			// 	tlGoTo.to(animatedObject, 0.25, {
+			// 		x: goToObj.x,
+			// 		y: goToObj.y,
+			// 		onUpdate( that ){
+
+			// 			that.computePosition({
+			// 				screenX: animatedObject.x,
+			// 				screenY: animatedObject.y
+			// 			})
+
+						
+			// 		},
+			// 		onUpdateParams: [this],
+			// 		onComplete( that ){
+			// 			that.isMovingFromZero = false;
+			// 		},
+			// 		onCompleteParams: [this],
+			// 	});
+
+			// },
 		}
 	}
 </script>
