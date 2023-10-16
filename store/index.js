b708aca1d1f9990600640999b55b8efb6d1a7c08
Lionel <joechipjoechip@gmail.com>
2023-10-16 13:10:25 +0200

    last fin tunings

--- store/index.js
+++ store/index.js
@@ -1,3 +1,10 @@
+
+// mac_vieux_safari: 	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15"
+// mac_vieux_chrome : 	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+// mac_new_safari : 	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15"
+// iphone_safari : 	"Mozilla/5.0 (iPhone; CPU iPhone OS 16_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1"
+
+
 export const state = () => ({
 	downScale: 1,
 	lastDownScale: 1,
